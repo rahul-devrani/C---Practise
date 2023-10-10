0cf10af353e3e06d0fd841430ab104136730bbd2
Rahul Devrani <142100974+rahul-devrani@users.noreply.github.com>
2023-10-10 19:20:13 +0530

    Add files via upload
    C Program to Swap Two Numbers

---
+++ Basic C programs/swap.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main(){
+    int a, b, temp;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("Before swapping: a = %d, b = %d\n", a, b);
+    temp = a;
+    a = b;
+    b = temp;
+    printf("After swapping: a = %d, b = %d", a, b);
+}

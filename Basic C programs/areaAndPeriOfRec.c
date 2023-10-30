20a3daf41ed70512a7fdf6b29e503099783eca19
Rahul Devrani <142100974+rahul-devrani@users.noreply.github.com>
2023-10-30 23:05:45 +0530

    Add files via upload

---
+++ Basic C programs/areaAndPeriOfRec.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ 
+int main()
+{
+ 
+    int l = 5, b = 6;
+    printf("Area of rectangle is : %d", l * b);
+    printf("\nPerimeter of rectangle is : %d", 2 * (l + b));
+    return 0;
+}
